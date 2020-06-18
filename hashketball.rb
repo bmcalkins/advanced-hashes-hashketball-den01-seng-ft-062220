fab5d598fee0d3a8fb2be6898f3bc0d0c53773fa
Bradley Calkins <b.raders1013@gmail.com>
2020-06-18 21:56:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,7 +146,7 @@ end
 def team_colors(name)
   game_hash.each do |team|
     if team[1][:team_name] == name
-      return team[1][:team_colors]
+      return team[1][:colors]
     end
   end  
 end
